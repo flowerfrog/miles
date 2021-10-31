8071962be2ba15950a7d2a355bb87255a290d115
Alexandra Osennyaya <alexosenn@gmail.com>
2021-10-31 11:39:24 +0300

    Miles.java added

---
+++ Miles.java
@@ -0,0 +1,8 @@
+public class Miles {
+    public static void main(String[] args) {
+        int amount = 2152;
+        int bonus = 20;
+        int miles = amount / bonus;
+        System.out.println("Количество начисленных миль: " + miles);
+    }
+}
